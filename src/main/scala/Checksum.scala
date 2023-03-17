4c11b883dc5cf608a633e82edbe0226de5cab43a
Jack Koenig <jack.koenig3@gmail.com>
2023-03-17 10:37:28 -0700

    Handle Cat moving from chisel3.util to chisel3 (#7)
    We specify the import in chisel3.util so that this code will compile in
    both Chisel 3.6 and earlier versions.

--- src/main/scala/Checksum.scala
+++ src/main/scala/Checksum.scala
@@ -1,7 +1,7 @@
 package icenet
 
 import chisel3._
-import chisel3.util._
+import chisel3.util.{Cat, _}
 import freechips.rocketchip.unittest.UnitTest
 import freechips.rocketchip.util.{DecoupledHelper, UIntIsOneOf}
 import testchipip.{StreamIO, StreamChannel}
